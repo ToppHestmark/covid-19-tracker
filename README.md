79fd7bbfc8dffdde27687c061f14303103bbfb85
Suphachai Hestmark <top_hestmark@yahoo.no>
2020-09-10 23:10:05 +0200

    Added chart to the sidebar

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # COVID-19 TRACKER
 
 - [API from disease.sh](https://disease.sh/)
+- React Hooks
+- Material-UI table
+- Chart: [react-chartjs-2](https://github.com/jerairrest/react-chartjs-2)
--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import InfoBox from './components/InfoBox';
 import Map from './components/Map';
 import Table from './components/Table';
 import { sortData } from './components/util';
+import LineGraph from './components/LineGraph';
 
 function App() {
   const [countries, setCountries] = useState([]);
@@ -103,6 +104,7 @@ function App() {
           <h3>Live cases by country</h3>
           <Table countries={tableData} />
           <h3>Worldwide new cases</h3>
+          <LineGraph />
         </CardContent>
       </Card>
         
