4aa8ba3279de11be6323643409109368a2b9fc55
Suphachai Hestmark <top_hestmark@yahoo.no>
2020-09-10 23:44:27 +0200

    Applied map and render it to the screen

--- src/App.js
+++ src/App.js
@@ -6,12 +6,16 @@ import Map from './components/Map';
 import Table from './components/Table';
 import { sortData } from './components/util';
 import LineGraph from './components/LineGraph';
+import "leaflet/dist/leaflet.css";
+
 
 function App() {
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState('worldwide');
   const [countryInfo, setCountryInfo] = useState({});
-  const [tableData, setTableData] = useState([])
+  const [tableData, setTableData] = useState([]);
+  const [mapCenter, setMapCenter] = useState({ lat: 34.80746, lng: -40.4796 });
+  const [mapZoom, setMapZoom] = useState(3)
 
   useEffect(() => {
     fetch("https://disease.sh/v3/covid-19/all")
@@ -96,7 +100,10 @@ function App() {
           total={countryInfo.deaths} />
         </div>
 
-        <Map />
+        <Map 
+          center={mapCenter}
+          zoom={mapZoom}
+        />
       </div>
       
       <Card className="app__right">
