d07805fc481621f5bf709fe9c3de3674197fd1d7
Suphachai Hestmark <top_hestmark@yahoo.no>
2020-09-10 15:08:36 +0200

    added variable in the App and attatch it with the dropdown

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import { MenuItem, FormControl, Select } from "@material-ui/core";
 
 function App() {
+  const [countries, setCountries] = useState([
+    'USA', 'INDIA', 'PAKISTAN'
+  ]);
+
   return (
     <div className="app">
-      
+      <div className="app__header">
+      <h1>Covid-19 Tracker</h1>
+        <FormControl className="app__dropdown">
+          <Select
+            variant="outlined"
+            value="abc"
+            >
+
+            {
+              countries.map(country => (
+                <MenuItem value={country}> {country} </MenuItem>
+              ))
+            }
+            
+            {/* <MenuItem value="worldwide">Worldwide</MenuItem>
+            <MenuItem value="worldwide">Option 1</MenuItem>
+            <MenuItem value="worldwide">Option 2</MenuItem>
+            <MenuItem value="worldwide">Option 3</MenuItem> */}
+
+          </Select>
+        </FormControl>
+      </div>
     </div>
   );
 }
